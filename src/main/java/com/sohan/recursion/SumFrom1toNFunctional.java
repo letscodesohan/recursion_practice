df57f00a89fe0c20b1516c0adc863cd030c6d32a
letscodesohan <lets.code.sohan@gmail.com>
2022-09-05 01:30:06 +0530

    second commit

---
+++ src/main/java/com/sohan/recursion/SumFrom1toNFunctional.java
@@ -0,0 +1,22 @@
+package com.sohan.recursion;
+
+
+import java.util.Scanner;
+
+//print sum 1 to N
+public class SumFrom1toNFunctional {
+    private static int n;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number :");
+        n = sc.nextInt();
+        System.out.println("Sum from 1 till " + n + " using functional approach is : " + printSumTillN(n));
+    }
+
+    public static int printSumTillN(int i){
+      if(i == 1)
+          return 1;
+      return i + printSumTillN(i-1);
+    }
+}
---
+++ src/main/java/com/sohan/recursion/SumFrom1toNParameterized.java
@@ -0,0 +1,24 @@
+package com.sohan.recursion;
+import java.util.Scanner;
+
+// print sum of 1 to N
+
+public class SumFrom1toNParameterized {
+    private static int n;
+    private static int sum = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number :");
+        n = sc.nextInt();
+        printSumTillN(n,sum);
+    }
+
+    public static void printSumTillN(int i, int sum){
+        if(i<1){
+            System.out.println("Sum from 1 till " + n + " is : " + sum);
+            return;
+        }
+        printSumTillN(i-1,sum+i);
+    }
+}
