cc6f955a826446c5c3b0ea3fcd978d2694b80efc
letscodesohan <lets.code.sohan@gmail.com>
2022-09-14 17:32:26 +0530

    recursion practice code session 2

---
+++ src/main/java/com/sohan/recursion/FactorialOfN.java
@@ -0,0 +1,22 @@
+package com.sohan.recursion;
+
+import java.util.Scanner;
+
+// print factorial of N using functional way
+public class FactorialOfN {
+
+    private static int n;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        n = sc.nextInt();
+        System.out.println("Factorial of " + n + " is : " + factorialOfN(n));
+    }
+
+    private static int factorialOfN(int n) {
+        if(n == 1)
+            return 1;
+        return n * factorialOfN(n-1);
+    }
+}
